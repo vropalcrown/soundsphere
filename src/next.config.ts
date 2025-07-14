3811466d41ae0dfbf93503b82aeea41ef8370a64
Devanandan K c <devanandankc2@gmail.com>
2025-07-14 07:40:36 +0000

    vercel shows this [13:08:10.208] Running build in Washington, D.C., USA

--- src/next.config.ts
+++ src/next.config.ts
@@ -5,7 +5,6 @@ import withPWA from '@ducanh2912/next-pwa';
 const pwaConfig = withPWA({
   dest: 'public',
   register: true,
-  skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
 });
 
